51eda227b14d1d7603f017901063913acf354634
etozhenerk <etozhenerk@yandex.ru>
2019-10-12 15:45:02 +0300

    последний день

--- script/script.js
+++ script/script.js
@@ -200,16 +200,50 @@ document.addEventListener("DOMContentLoaded", () => {
       const gloTube = document.querySelector(".logo-academy");
       const trends = document.querySelector("#yt_trend");
       const like = document.querySelector("#yt_like");
+      const subscriptions = document.querySelector("#yt_subscriptions");
+      const searchForm = document.querySelector(".search-form");
 
       const request = options => gapi.client.youtube[options.method]
         .list(options)
         .then(response => response.result.items)
-        .then(render)
-        .then(youtuber)
+        .then(data => options.method === "subscriptions" ?
+        renderSub(data) : render(data))
         .catch(err => console.error("Во время запроса произошла ошибка: " + err));
 
+      const renderSub = data =>{
+        const ytWrapper = document.querySelector("#yt-wrapper");
+        ytWrapper.textContent = '';
+        data.forEach(item =>{
+          try {
+            const {snippet:{resourceId:{channelId}, description, title, thumbnails:{high:{url}}}} = item;
+            ytWrapper.innerHTML += `
+            <div class="yt" data-youtuber="${channelId}">
+              <div class="yt-thumbnail" style="--aspect-ratio:16/9;">
+                <img src="${url}" alt="thumbnail" class="yt-thumbnail__img">
+              </div>
+              <div class="yt-title">${title}</div>
+              <div class="yt-channel">${description}</div>
+          </div>
+            `;
+        } catch (err) {
+          console.error(err);
+        }
+        });
+
+        ytWrapper.querySelectorAll(".yt").forEach(item =>{
+          item.addEventListener("click", () =>{
+            request({
+              method: "search",
+              part: "snippet",
+              channelId: item.dataset.youtuber,
+              order: "date",
+              maxResults: 6,
+            });
+          });
+        });
+      };
+
       const render = data =>{
-        console.log(data);
         const ytWrapper = document.querySelector("#yt-wrapper");
         ytWrapper.textContent = '';
         data.forEach(item =>{
@@ -228,6 +262,8 @@ document.addEventListener("DOMContentLoaded", () => {
           console.error(err);
         }
         });
+
+        youtuber();
       };
 
       gloTube.addEventListener("click", () => {
@@ -259,6 +295,36 @@ document.addEventListener("DOMContentLoaded", () => {
         });
       });
 
+      subscriptions.addEventListener("click", ()=>{
+        request({
+          method: "subscriptions",
+          part: "snippet",
+          mine: true,
+          maxResults: 6,
+        });
+      });
+
+      searchForm.addEventListener("submit", e =>{
+        e.preventDefault();
+
+        const valueInput = searchForm.elements[0].value;
+
+        if(!valueInput) {
+          searchForm.style.border = "1px solid red";
+          return;
+          }
+
+        searchForm.style.border = "";
+        request({
+          method: "search",
+          part: "snippet",
+          order: "relevance",
+          maxResults: 6,
+          q: valueInput,
+        });
+
+        searchForm.elements[0].value = "";
+      });
     }
   }
 });
